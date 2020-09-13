75c8585c5c9ea3b3c8c2e0277fae2d7c0e18c2cd
leandregrimmel <leandregrimmel@gmail.com>
2020-09-13 21:29:55 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import string 
+def majuscule(mot):
+    mot_output = ''
+    for lettre in mot:
+        lettre = ord(lettre) - 32
+        mot_output += chr(lettre)
+    return mot_output
 
-def capitaliser_pays(nom):
-    nom[0:].lower
-    return nom
 
- 
 if __name__ == '__main__':
-    pays = [
-        'AfghanIstan',
-        'albania',
-        'algeria',
-        'AndorRa',
-        'angolA',
-        'antigua ANd barbuda',
-        'argEntina',
-        'Armenia',
-        'austrAlia',
-        'ausTria',
-        'azerBaijaN'
+    mots = [
+        'riz',
+        'cours',
+        'voiture',
+        'oiseau',
+        'bonjour',
+        'églantier',
+        'arbre',
+        'yolo'
     ]
-    for i in range(len(pays)):
-        pays[i] = capitaliser_pays(pays[i])
+    for i in range(len(mots)):
+        mots[i] = majuscule(mots[i])
 
-    print(pays)
+    print(mots)
